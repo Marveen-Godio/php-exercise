7fe5b9817c0660e0cc8bdb7224bcab250e0c042b
Godio-Hunot Marveen <godio.marveen@gmail.com>
2019-10-20 17:13:31 +0200

    exercices array

---
+++ array/array4.php
@@ -0,0 +1,17 @@
+<?php
+$m = array(
+    1 =>'Janvier',
+    2 => 'Fevrier',
+    3 => 'Mars',
+    4 => 'Avril',
+    5 => 'Mai', 
+    6 => 'Juin', 
+    7 => 'Juillet', 
+    8 => 'Aout', 
+    9 => 'Septembre', 
+    10 => 'Octobre', 
+    11 => 'Novembre', 
+    12 => 'Décembre');
+$m[8] = 'août';
+    var_dump($m[8]);
+?>
---
+++ array/array5.php
@@ -0,0 +1,9 @@
+<?php
+$m = array(
+    02 =>'aisne',
+    59 => 'nord',
+    60 => 'oise',
+    62 => 'Pas de Calai',
+    80 => 'somme');
+var_dump($m);
+?>
